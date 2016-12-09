5c06ba6628281eb457ff91837975af27bd9650d0
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-12-09 07:47:44 +0300

    Use math operators

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(37);
+        display(77* 2 + 1);
     }
 
     /**
