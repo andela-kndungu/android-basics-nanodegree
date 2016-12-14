7a04fd4f3bfa399655fc5443cc503fb76ce2fdfa
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-12-14 15:10:07 +0300

    New value based on old

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         int quantity = 2;
-        quantity = 3;
+        quantity = quantity + 1;
         display(quantity);}
 
     /**
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         int quantity = 2;
-        quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 }
